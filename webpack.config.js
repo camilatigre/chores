cba5ee52f49e93282b785337b3cbf8aa08eac5d3
Camila Tigre <camila.tigre@arizona.global>
2017-06-08 12:58:18 -0300

    config webpack

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+var path = require('path');
+
+module.exports = {
+    entry: './app/index.js',
+    output: {
+        path: path.resolve(__dirname, 'public'),
+        filename: 'bundle.js'
+    },
+
+    devServer: {
+      contentBase: './public',
+      inline: true,
+      port: 9000
+    },
+    module: {
+        loaders: [{
+            test: /\.js$/,
+            exclude: /node_modules/,
+            loader: 'babel-loader',
+            query: {
+                presets: ['es2015', 'react']
+            }
+        }]
+    }
+}
